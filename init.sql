c568ad3e60acadc598b64265df8d27b4173a7bcf
Dan Mi <mdatgm@gmail.com>
2024-09-27 07:44:37 +0000

    update golf manager

--- init.sql
+++ init.sql
@@ -3,4 +3,11 @@ CREATE TABLE courses (
   name VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE bookings (
+  id SERIAL PRIMARY KEY,
+  course_id INTEGER NOT NULL REFERENCES courses(id),
+  user_name VARCHAR(100) NOT NULL,
+  booking_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 INSERT INTO courses (name) VALUES ('阳光高尔夫俱乐部'), ('月光高尔夫度假村');
